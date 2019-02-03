218f2cbfe2cc0853eeae8603a72770b1078fc029
Zoltan Dalmadi <dmz985@gmail.com>
2019-02-03 15:20:06 +0100

    [main] Adapt config changes

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const logger = require('pino')();
+const dbLogger = logger.child({ module: 'db' });
 const apiLogger = logger.child({ module: 'api' });
 const moviedbLogger = logger.child({ module: 'moviedb' });
 const syncLogger = logger.child({ module: 'sync' });
 const imageDownloaderLogger = logger.child({ module: 'image_downloader' });
 
+// TODO: read this from yaml
 const config = {
-  imagesFolder: './images',
-  watchedFolder: './watched'
+  dbPath: '.',
+  dbSchema: './db_schema.sql',
+  port: 3000,
+  watchedFolder: './watched',
+  moviedb: {
+    apiKey: '3709d7c83bb78de89f9087776b362df0'
+  }
 };
 
-const db = require('./src/db');
+// init modules
+const db = require('./src/db')(dbLogger, config);
 const api = require('./src/api')(db, apiLogger);
-const moviedb = require('./src/moviedb')(moviedbLogger);
+const moviedb = require('./src/moviedb')(moviedbLogger, config);
 const imageDownload = require('./src/image')(imageDownloaderLogger);
 const sync = require('./src/sync')(db, moviedb, imageDownload, syncLogger, config);
 
 sync.start()
   .then(() => {
     sync.watch();
-    api.listen(3000);
+    api.listen(config.port);
   });
